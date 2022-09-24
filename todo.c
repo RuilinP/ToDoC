f061f50e72e24c9bea685403c4fa841127b2670f
RuilinP <ruilin.peng@mail.utoronto.ca>
2022-09-24 15:02:38 -0400

    First task can be added

--- todo.c
+++ todo.c
@@ -21,13 +21,13 @@ int main()
 {
         int choice;
         interface();
-
+        printf("\n");
         while (1) {
 
-   
-                printf("1. To see your ToDo list\n");
-                printf("2. To create new ToDo\n");
-                printf("3. To delete your ToDo\n");
+                printf("\n \n");
+                printf("1. To see today's ToDo list\n");
+                printf("2. To create a new ToDo\n");
+                printf("3. To delete a ToDo\n");
                 printf("4. Exit");
                 printf("\n\n\nEnter your choice\t:\t");
 
@@ -36,10 +36,10 @@ int main()
                 switch (choice) {
 
                 case 1:
-                        //seetodo();
+                        seetodo();
                         break;
                 case 2:
-                        //createtodo();
+                        createtodo();
                         break;
                 case 3:
                         //deletetodo();
@@ -49,14 +49,19 @@ int main()
                         break;
                 default:
                         printf("\nInvalid Choice :-(\n");
-                        //system("pause");
+                        break;
                 }
         }
         return 0;
 }
 
 void interface()
-{
+{   
+    printf("~~~~~~~~~~~~~~~~~~~~~"
+           "~~~~~~~~~~~~~~~~~~~~~~~~"
+           "~~~~~~~~~~~~~~~~~~~~~~~~"
+           "~~~~~~~~~~~~~~~~~~~~~~~"
+           "~~~~~~~~~~\n");
     printf("Welcome to \n \n");
     printf("\n");
     printf("#######        ######          #####  \n");
@@ -64,8 +69,36 @@ void interface()
     printf("   #    #    # #     # #    # #       \n");
     printf("   #    #    # #     # #    # #       \n");
     printf("   #    #    # #     # #    # #     # \n");
-    printf("   #     ####  ######   ####   ##### \n");
+    printf("   #     ####  ######   ####   #####  V 0.1\n");
     printf(" \n \n");
     printf("\t \t \t by RuilinP\n");
     printf("\n \n ");
+}
+
+void seetodo(){
+     process* temp;
+     temp = start;
+     if(start == NULL){
+        printf("Your todo List is empty! Cheer!\n");
+     }
+
+     while (temp != NULL){
+           printf("%d.", temp->count);
+           printf(temp->buffer);
+           temp = temp->next;
+     }
+     printf("\n \n \n ");
+}
+
+void createtodo(){
+     process* add, temp;
+  
+     if(start == NULL){
+        add = (process*)malloc(sizeof(process));
+        start = add;
+        printf("\n input your first task:");
+        scanf("%50s", add->buffer);         
+        add->count = 1;
+        start->next = NULL;
+     }
 }
